fb7ba102aa2bb8d71e35dae9c771fac6bbcd12c8
Vladislav Kostikov <vlad@kostikov.ru>
2024-06-07 05:22:13 +0300

    Исправил опечатки

--- pact/refactoring.rb
+++ pact/refactoring.rb
@@ -1,20 +1,34 @@
+# User object in database
+# name string
+# surname string
+# patronymic string
+# fullname string
+# email string
+# age integer
+# nationality string
+# country string
+# interests array
+# gender string
+# skills string
 class User < ApplicationRecord
   has_many :interests
   has_many :skills, class_name: 'Skil'
 end
 
+# Interest object in database
+# name string
 class Interest < ApplicationRecord
   has_many :users
 end
 
+# Skil object in database
+# name string
 class Skil < ApplicationRecord
   has_many :users
 end
 
-#In appliaction we are using ActiveInteraction gem => https://github.com/AaronLasseigne/active_interaction
+# In application we are using ActiveInteraction gem => https://github.com/AaronLasseigne/active_interaction
 class Users::Create < ActiveInteraction::Base
-  hash :params
-
   def execute
     #don't do anything if params is empty
     return unless params['name']
@@ -27,13 +41,13 @@ class Users::Create < ActiveInteraction::Base
     ##########
     return if User.where(email: params['email'])
     return if params['age'] <= 0 || params['age'] > 90
-    return if params['gender'] != 'male' or params['gender'] != female
+    return if params['gender'] != 'male' || params['gender'] != 'female'
 
     user_full_name = "#{params['surname']} #{params['name']} #{params['patronymic']}"
     user_params = params.except(:interests)
     user = User.create(user_params.merge(user_full_name))
 
-    Intereset.where(name: params['interests']).each do |interest|
+    Interest.where(name: params['interests']).each do |interest|
       user.interests = user.interest + interest
       user.save!
     end
@@ -41,28 +55,9 @@ class Users::Create < ActiveInteraction::Base
     user_skills = []
     params['skills'].split(',').each do |skil|
       skil = Skil.find(name: skil)
-      user_skills =  user_skills + [skil]
+      user_skills += [skil]
     end
     user.skills = user_skills
     user.save
   end
 end
-
-
-#User object in database
-name string
-surname string
-patronymic string
-fullname string
-email string
-age integer
-nationality string
-country string
-interests array
-gender string
-skills string
-
-#Interest object in database
-name string
-#Skil oject in database
-name string
